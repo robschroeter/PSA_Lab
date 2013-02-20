37bdccd1c7fb9e4055d83936c0da315ffd29696b
Carl Godon <Carl.Godon@CatalinaMarketing.com>
2013-02-20 08:46:46 -0500

    carl3 update

--- mongoPsa/src/main/java/com/cuesol/cc/App.java
+++ mongoPsa/src/main/java/com/cuesol/cc/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "CArl brnch 2 after swtiching back to heqad" );
+        System.out.println( "after 1 adn 2 merged to head, go back to 2 and create branch 3" );
     }
 }
