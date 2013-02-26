e53d455bf329d77b9d91a9e9eb32b3df2664dc97
avinash <Avinash.Koneru@catalinamarketing.com>
2013-02-26 10:49:46 -0500

    creating new branch

---
+++ mongoPsa/src/main/java/com/cuesol/cc/Testing.java
@@ -0,0 +1,19 @@
+package com.cuesol.cc;
+
+public class Testing {
+
+	private int value;
+	
+	public Testing(int value){
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+}
